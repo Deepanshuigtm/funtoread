6a4f09094dec408ec6f1578f1f76747941b4a457
Deepanshuigtm <91874023+Deepanshuigtm@users.noreply.github.com>
2024-01-23 16:15:49 +0530

    added conditional changes in navbar

--- lib/screens/main_dashboard.dart
+++ lib/screens/main_dashboard.dart
@@ -9,32 +9,39 @@ class MainDashboard extends StatelessWidget{
   MainDashboard( {super.key} );
   String username = "Shubham";
   int currentBalance = 1000;
+  String page = "all_offers";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue.shade50,
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(70),
-          child: AppBarDashboard(username: username, currentBalance: currentBalance,),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              NavBar(),
-              SizedBox(height: 20,),
-              Padding(
-                padding: EdgeInsets.only(left: 12),
-                child: TrendingOffers(),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 12),
-                child: MoreOffers(),
-              ),
-            ],
+          backgroundColor: Colors.blue.shade50,
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(70),
+            child: AppBarDashboard(username: username, currentBalance: currentBalance,),
           ),
-        )
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                NavBar(page: page,),
+                const SizedBox(height: 20,),
+                page == 'all_offers' ?
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 12),
+                      child: TrendingOffers(),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      child: MoreOffers(),
+                    ),
+                  ],
+                ):
+                const Text('Other pages')
+              ],
+            ),
+          )
       ),
     );
   }
--- lib/widgets/nav_bar.dart
+++ lib/widgets/nav_bar.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class NavBar extends StatefulWidget {
-  NavBar({ Key? key }) : super(key: key);
+  final String page;
+  const NavBar({ Key? key, required this.page }) : super(key: key);
 
   @override
   _NavBarState createState() => _NavBarState();
@@ -16,48 +17,93 @@ class _NavBarState extends State<NavBar> {
         borderRadius: BorderRadius.circular(10),
         color: Colors.white, // You can set your desired color
       ),
-      child: const Row(
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Icon(Icons.dashboard_customize_outlined, color: Colors.lightBlue),
-              SizedBox(height: 5.0),
-              Text('All Offers')
-            ],
+          Container(
+            width: 90,
+            decoration: BoxDecoration(
+              color: widget.page == 'all_offers'?Colors.black12:Colors.white,
+              border: Border(
+                bottom: BorderSide(
+                  color: widget.page == 'all_offers'?Colors.blue:Colors.white,
+                  width: 2.3,
+                ),
+              ),
+            ),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(Icons.dashboard_customize_outlined, color: Colors.lightBlue),
+                SizedBox(height: 5.0),
+                Text('All Offers')
+              ],
+            ),
           ),
-          SizedBox(width: 40,),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Icon(Icons.card_giftcard_outlined, color: Colors.red),
-              SizedBox(height: 5.0),
-              Text('Gifts')
-            ],
+          Container(
+            width: 90,
+            decoration: BoxDecoration(
+              color: widget.page == 'gifts'?Colors.black12:Colors.white,
+              border: Border(
+                bottom: BorderSide(
+                  color: widget.page == 'gifts'?Colors.blue:Colors.white,
+                  width: 2.3,
+                ),
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(Icons.card_giftcard_outlined, color: Colors.red),
+                SizedBox(height: 5.0),
+                Text('Gifts')
+              ],
+            ),
           ),
-          SizedBox(width: 40,),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Icon(Icons.update, color: Colors.orange),
-              SizedBox(height: 5.0),
-              Text('Upcoming')
-            ],
+          Container(
+            width: 90,
+            decoration: BoxDecoration(
+              color: widget.page == 'up_comming'?Colors.black12:Colors.white,
+              border: Border(
+                bottom: BorderSide(
+                  color: widget.page == 'up_comming'?Colors.blue:Colors.white,
+                  width: 2.3,
+                ),
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(Icons.update, color: Colors.orange),
+                SizedBox(height: 5.0),
+                Text('Upcoming')
+              ],
+            ),
           ),
-          SizedBox(width: 40,),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Icon(Icons.check_circle_outline, color: Colors.purple),
-              SizedBox(height: 5.0),
-              Text('My Offers')
-            ],
+          Container(
+            width: 90,
+            decoration: BoxDecoration(
+              color: widget.page == 'my_offers'?Colors.black12:Colors.white,
+              border: Border(
+                bottom: BorderSide(
+                  color: widget.page == 'my_offers'?Colors.blue:Colors.white,
+                  width: 2.3,
+                ),
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(Icons.check_circle_outline, color: Colors.purple),
+                SizedBox(height: 5.0),
+                Text('My Offers')
+              ],
+            ),
           ),
         ],
       ),
